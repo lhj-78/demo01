bf16b5d2b9d9af01369deb8fd407de8ff62bfcce
lhj-78 <2144158394@qq.com>
2025-12-11 21:38:27 +0800

    修改了管理员界面

--- models.py
+++ models.py
@@ -19,7 +19,7 @@ class User(UserMixin, db.Model):
     student_info = db.relationship('Student', backref='user', uselist=False)
 
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password, method='pbkdf2:sha256')
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
